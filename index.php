72f8a2d3436ebb3c77af01c10e07578776c6054d
Joffo40 <142779100+Joffo40@users.noreply.github.com>
2025-11-13 18:54:30 +0100

    Mise à Jour 2

--- index.php
+++ index.php
@@ -94,10 +94,10 @@
                     <div class="base">
                         <div class="competences"><p><h2>Compétences:</h2>
                         <ul>
-                            <li><Strong>Languages Informatiques:</Strong> HTML, CSS, PHP, SYMFONY, JAVASCRIPT, GIT, C, ADA, VISUAL BASIC (formation année 2006 et utilisation).</li>
-                            <li><Strong>Outils AIRBUS:</Strong> SAP (formation année 2016 et utilisation), ACSA, PSN, SAFE, ISAMI, ASSIST, ASSIST-LINK.</li>
+                                                        <li><Strong>Outils AIRBUS:</Strong> SAP (formation année 2016 et utilisation), ACSA, PSN, SAFE, ISAMI, ASSIST, ASSIST-LINK.</li>
                             <li><Strong>Outils DASSAULT:</Strong> Outils Calcul Dassault (Statique/Fatigue et Tolerance aux dommages), NASGRO, CATIA FE (formation année 2017 et utilisation)</li>
                             <li><Strong>Outils CALCUL/DESIGN:</Strong> PATRAN/NASTRAN,Advance Design GRAITEC(formation année 2024), CADLINK (formation année 2006 et utilisation), CADDS 5, CATIAV5, EFFEL, AUTOCAD2000.</li>
+                            <li><Strong>Languages Informatiques:</Strong> VISUAL BASIC (formation année 2006 et utilisation), HTML, CSS, PHP, SYMFONY, JAVASCRIPT, GIT, C, ADA.</li>
                             <li><Strong>Outils OFFICE:</Strong> EXCEL, WORD, POWERPOINT, VISUAL BASIC (formation année 2006 et utilisation).</li>
                             <li><Strong>Langues:</Strong> Anglais (TOEIC Listening & Reading: 825 – 12/2013), Espagnol Scolaire.</li>
                         </ul>
